fbd5b0578065bdd51e7564e839406c4e1bbdfb2d
Nihal Sabri <Nihalsabri60@gmail.com>
2025-02-22 20:22:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base : 'Blog',
+    build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  },
+  // Hook to generate 404.html
+  buildEnd: () => {
+    fs.copyFileSync('dist/index.html', 'dist/404.html');
+  }
+});
+
